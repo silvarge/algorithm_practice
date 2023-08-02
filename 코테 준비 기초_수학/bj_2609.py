7cff76eb16254c98826f470555cd6b3ef72f1549
silvarge <sep.non2@gmail.com>
2023-08-02 22:46:03 +0900

    파일 제목 수정

---
+++ 코테 준비 기초_수학/bj_2609.py
@@ -0,0 +1,22 @@
+n = list(map(int, input().split()))
+n.sort(reverse=True)
+
+b = n[0]
+gcd = n[1]
+lcm = 1
+
+# 최대공약수
+while True:
+    if (b%gcd != 0):
+        temp = b%gcd
+        b = gcd
+        gcd = temp
+    else:
+        break
+
+
+# 최소공배수
+lcm = (n[0] * n[1])//gcd
+
+print(gcd)
+print(lcm)
